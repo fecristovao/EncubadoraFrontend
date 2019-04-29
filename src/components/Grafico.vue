9ff796e9a99f4c57f50c15944f0e23939ac349d7
Fernando Cristovao <fernandocristovao@id.uff.br>
2019-04-28 21:11:12 -0300

    Frontend com gráficos da encubadora

---
+++ src/components/Grafico.vue
@@ -0,0 +1,61 @@
+<script>
+import { Line, mixins } from 'vue-chartjs'
+
+export default {
+    name: 'Grafico',
+    extends: Line,
+    mixins: [mixins.reactiveProp],
+
+    data() {
+        return {
+            chartdata: {
+                labels: this.dias,
+                datasets: [{
+                    label: 'Temperatura',
+                    fill: false,
+                    backgroundColor: "#ff0000",
+                    borderColor: "#ff0000",
+                    data: this.temperatura
+                },
+                {
+                    label: 'Umidade',
+                    fill: false,
+                    backgroundColor: "#0000cc",
+                    borderColor: "#0000cc",
+                    data: this.umidade
+                }]
+            },
+            options: {
+				responsive: true,
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: ''
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+                            display: true,
+                            labelString: ''
+					    }
+				    }]
+			    }
+		    }
+        }
+    },
+    mounted() {
+        this.renderChart(this.chartData, this.options)
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,161 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="tudo">
+    <div id="data">
+      Data Inicio: <datepicker v-model="inicio"></datepicker>
+      Data Fim: <datepicker v-model="fim"></datepicker>
+    </div>
+    <Grafico id="gr" :chartData="chartData3"></Grafico>
+    <div id="grafico">
+      <Grafico id="grafico1" :chartData="chartData"></Grafico>
+      <Grafico :chartData="chartData2"></Grafico><br>
+      
+    </div>
+    
   </div>
+  
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Grafico from '@/components/Grafico.vue'
+import axios from 'axios'
+import { setInterval } from 'timers';
+import Datepicker from 'vuejs-datepicker';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Grafico, Datepicker
+  },
+  data() {
+    return {
+      loaded: false,
+      horas: [],
+      temperatura: [],
+      umidade: [],
+      inicio: new Date(),
+      fim: new Date(),
+      chartData: {},
+      chartData2: {},
+      chartData3: {}
+    }
+  },
+  computed: {
+    dataInicio() {
+      return this.formatarData(this.inicio)
+    },
+    dataFim() {
+      return this.formatarData(this.fim)
+    }
+  },
+  methods: {
+    formatarData(data) {
+        var dia  = data.getDate().toString().padStart(2, '0')
+        var mes  = (data.getMonth()+1).toString().padStart(2, '0') //+1 pois no getMonth Janeiro começa com zero.
+        var ano  = data.getFullYear()
+        return dia+"-"+mes+"-"+ano;
+    },
+    pegarDados() {
+      console.log("pegando dados")
+      this.loaded = false
+      this.umidade = []
+      axios.get('http://encubensis.herokuapp.com/data/'+this.dataInicio+'/'+this.dataFim).then(response => {
+        // JSON responses are automatically parsed.
+        console.log(response)
+        this.temperatura = response.data.map(t => {return t.temperatura != null ? t.temperatura : 0})
+        this.umidade = response.data.map(u => {return u.umidade != null ? u.umidade : 0})
+        var k = this;
+        this.horas = response.data.map(hora => {
+          var data = new Date(hora.created_at)
+          return (data.getHours() + ":" + data.getMinutes()).length < 5 ?  (data.getHours() + ":0" + data.getMinutes()) :  (data.getHours() + ":" + data.getMinutes())
+          })
+      }).catch(e => {
+        this.errors.push(e)
+      }).finally(() => {
+        this.chartData = {
+                labels: this.horas,
+                datasets: [{
+                    label: 'Temperatura',
+                    fill: false,
+                    backgroundColor: "rgb(255, 99, 132)",
+                    borderColor: "rgb(255, 99, 132)",
+                    data: this.temperatura
+                }]
+          }
+        this.chartData2 = {
+                labels: this.horas,
+                datasets: [
+                {
+                    label: 'Umidade',
+                    fill: false,
+                    backgroundColor: "rgb(54, 162, 235)",
+                    borderColor: "rgb(54, 162, 235)",
+                    data: this.umidade
+                }]
+          }
+
+        this.chartData3 = {
+                labels: this.horas,
+                datasets: [
+                {
+                    label: 'Umidade',
+                    fill: false,
+                    backgroundColor: "rgb(54, 162, 235)",
+                    borderColor: "rgb(54, 162, 235)",
+                    data: this.umidade
+                },{
+                    label: 'Temperatura',
+                    fill: false,
+                    backgroundColor: "rgb(255, 99, 132)",
+                    borderColor: "rgb(255, 99, 132)",
+                    data: this.temperatura
+                }]
+          }
+      })
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.pegarDados()
+    }, 5000);
   }
 }
 </script>
+
+<style scoped>
+  #tudo {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-self: center;
+    text-align: center;
+    justify-items: center;
+    justify-self: center;
+    flex-wrap: wrap;
+  }
+  #grafico {
+    display: flex;
+    flex-direction: row;
+    justify-items: center;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  #gr {
+    width: 70%;
+  }
+  #data {
+     border: 1px solid #ddd;
+  padding: 0em 1em 1em;
+  display: flex;
+    flex-direction: row;
+  margin-bottom: 2em;
+  }
+
+  #data p {
+    padding: 0;
+    margin:0;
+  }
+  
+</style>
